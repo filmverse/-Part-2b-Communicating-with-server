1b6743f0dd53923e4124496112cff2ed9e0e8f94
vikas <vkassharma19492@gmail.com>
2023-01-22 01:27:35 +0530

    first version of notes app has been added/updated

--- part1b/src/App.js
+++ part1b/src/App.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
 
 const App = () => {
+
+  const [ notes, setNotes ] = useState([])
+
+  console.log(notes)
+
+  const hook = () => {
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log(response.data)
+      setNotes(response.data)
+    })
+  }
+  useEffect(hook, [])
+
   return (
-    <div></div>
+    <div>
+      <ul>
+        {notes.map(notemap => <Note key={notemap.id} note={notemap} />)}
+      </ul>
+    </div>
   )
 }
 
