b34aa553a6b951558201c032d934c18c53a9f4d3
vikas <vkassharma19492@gmail.com>
2023-01-22 03:32:14 +0530

    add note functionality has been added

--- part1b/src/App.js
+++ part1b/src/App.js
@@ -5,6 +5,7 @@ import Note from "./components/Note";
 const App = () => {
 
   const [ notes, setNotes ] = useState([])
+  const [ newNote, setNewNote ] = useState('')
 
   console.log(notes)
 
@@ -16,11 +17,37 @@ const App = () => {
   }
   useEffect(hook, [])
 
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random < 0.5,
+    }
+    axios.post('http://localhost:3001/notes', noteObject).then(
+      response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      }
+    )
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
+    <h1>Notes</h1>
+      <button>show important</button>
       <ul>
         {notes.map(notemap => <Note key={notemap.id} note={notemap} />)}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      debug: {newNote}
     </div>
   )
 }
