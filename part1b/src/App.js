8c3b55f75fcbec9ca54fa5281c7f3c902763c9d6
vikas <vkassharma19492@gmail.com>
2023-01-22 23:27:43 +0530

    note app has been updated

--- part1b/src/App.js
+++ part1b/src/App.js
@@ -40,6 +40,16 @@ const App = () => {
 
   console.log(showAll)
 
+  const toggleImportanceOf = (id) => {
+    // console.log(`importance of ${id} needs to be toggled`)
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   return (
     <div>
     <h1>Notes</h1>
@@ -47,7 +57,11 @@ const App = () => {
         show {showAll ? 'important' : 'all'}
       </button>
       <ul>
-        {notesToShow.map(notemap => <Note key={notemap.id} note={notemap} />)}
+        {notesToShow.map(notemap => <Note
+          key={notemap.id}
+          note={notemap}
+          toggleImportance={() => toggleImportanceOf(notemap.id)}
+        />)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
--- part1b/src/components/Note.js
+++ part1b/src/components/Note.js
@@ -1,8 +1,12 @@
 
-const Note = ({ note }) => {
+const Note = ({ note, toggleImportance }) => {
+
+    const label = note.important
+        ? 'make not important' : 'make important'
+
     return (
         <div>
-            {note.content} <button>testing</button>
+            {note.content} <button onClick={toggleImportance}>{label}</button>
         </div>
     )
 }
