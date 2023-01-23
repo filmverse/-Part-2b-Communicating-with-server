2ba895d3297780712b99a2fcc68fc9a1efbcaa1e
vikas <vkassharma19492@gmail.com>
2023-01-24 01:07:23 +0530

    phonebook app has been updated

--- part2b/src/App.js
+++ part2b/src/App.js
@@ -5,6 +5,8 @@ import Person from "./components/Person";
 const App = () => {
 
   const [ persons, setPersons ] = useState([])
+  const [ personName, setPersonName ] = useState("")
+  const [ personNumber, setPersonNumber ] = useState("")
 
   const hook = () => {
     axios.get('http://localhost:3001/persons').then(
@@ -15,18 +17,22 @@ const App = () => {
   }
   useEffect(hook, [])
 
+  const handleChange = (setValue) => (event) => setValue(event.target.value)
+
   return (
     <div>
       <h1>Phonebook</h1>
       <h2>add a new</h2>
       <form>
-        name: <input /><br />
-        number: <input /><br />
+        name: <input value={personName} onChange={handleChange(setPersonName)} /><br />
+        number: <input value={personNumber} onChange={handleChange(setPersonNumber)} /><br />
         <button>add</button>
       </form>
       {persons.map(person => (
         <Person key={person.id} phoneBookPerson={person} />
       ))}
+      Debug name: {personName}<br />
+      Debug number: {personNumber}
     </div>
   )
 }
