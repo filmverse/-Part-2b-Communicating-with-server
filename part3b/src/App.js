431d306b927f0ae0ab7c02de51e7f585766583c7
vikas <vkassharma19492@gmail.com>
2023-01-31 17:07:54 +0530

    country app has been created

--- part3b/src/App.js
+++ part3b/src/App.js
@@ -4,22 +4,33 @@ import axios from "axios";
 const App = () => {
 
   const [ countries, setCountries ] = useState([])
+  const [ searchQuery, setSearchQuery ] = useState("")
 
   const hook = () => {
     axios.get('https://restcountries.com/v3.1/all').then(
       response => {
         console.log(response.data)
-        setCountries(response.data)
+        setCountries(response.data.map(
+          country => ({
+            name: country.name.common,
+            capital: country.capital,
+            area: country.capital,
+            language: country.languages,
+            flag: country.flag,
+            flags: country.flags
+          })
+        ))
       }
     )
   }
   useEffect(hook, [])
 
+  const handleChange = (setValue) => (event) => setValue(event.target.value)
+
   return (
     <div>
-      {countries.map(country => (
-        <p key={country.name.common}>{country.name.common}</p>
-      ))}
+      <p>find countries <input value={searchQuery} onChange={handleChange(setSearchQuery)} /></p>
+      {countries.map(cname => <p key={cname.name}>{cname.name}</p>)}
     </div>
   )
 }
